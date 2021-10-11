956931d655e9cf5561e6ce4fa2daee8f44573bc4
Hugh Harper <hughaharper@gmail.com>
2021-10-11 16:16:47 -0700

    Modify train-test split routine

--- train-test-split/chunk-data.py
+++ train-test-split/chunk-data.py
@@ -1,38 +1,41 @@
 import os
 import random
+import pickle
 
 
-data_dir = "/cryosat3/btozer/CREATE_ML_FEATURES/tsv_all"
-write_dir = "/cryosat3/jalafate/chunk_tsv"
-regions = ['JAMSTEC', 'JAMSTEC2', 'NGDC', 'SIO', 'US_multi']
+data_dir = "/cryosat2/ML_DATA/runtime_data"
+write_dir = "/cryosat2/ML_DATA/runtime_data_chunks"
+regions = ['AGSO', 'JAMSTEC', 'JAMSTEC2', 'NGA', 'NGA2', 'NGDC', 'NOAA_geodas', 'SIO', 'US_multi']
 
 os.mkdir(write_dir)
-for region in regions:
-    os.mkdir(os.path.join(write_dir, region))
+#for region in regions:
+#os.mkdir(os.path.join(write_dir, region))
 
-    dirname = os.path.join(data_dir, region)
-    ext = ".tsv"
-    filenames = [filename for filename in os.listdir(dirname) if filename.endswith(ext)]
+#dirname = os.path.join(data_dir, region)
+#ext = ".tsv"
+dirname = data_dir
+ext = ".tsv.pkl"
+filenames = [filename for filename in os.listdir(dirname) if filename.endswith(ext)]
 
-    chunk_size = 100000
-    for filename in filenames:
-        basename = filename[:-4]
-        path = os.path.join(dirname, filename)
-        with open(path) as f:
-            data = f.readlines()
+chunk_size = 100000
+for filename in filenames:
+    basename = filename[:-8]
+    path = os.path.join(dirname, filename)
+    with open(path,'rb') as f:
+        data, labels, weights = pickle.load(f)
 
-        num_line = len(data)
-        cursor = 0
-        part = 0
-        while cursor < num_line:
-            start, end = cursor, cursor + chunk_size
-            if (num_line - end) * 2 < chunk_size:
-                end = num_line
-            cursor = end
+    num_line = len(data)
+    cursor = 0
+    part = 0
+    while cursor < num_line:
+        start, end = cursor, cursor + chunk_size
+        if (num_line - end) * 2 < chunk_size:
+            end = num_line
+        cursor = end
 
-            part_filename = basename + ".part{}.tsv".format(part)
-            part += 1
-            write_loc = os.path.join(write_dir, region, part_filename)
-            with open(write_loc, "w") as f:
-                f.write("".join(data[start:end]))
-
+        part_filename = basename + ".part{}.tsv.pkl".format(part)
+        part += 1
+        write_loc = os.path.join(write_dir, part_filename)
+        with open(write_loc, "wb") as f:
+            pickle.dump((data[start:end],labels[start:end],weights[start:end]),f,protocol=4)
+            #f.write("".join(data[start:end]))
--- train-test-split/train-test-split.py
+++ train-test-split/train-test-split.py
@@ -2,19 +2,47 @@ import os
 import random
 
 
-data_dir = "/path/to/the/root/of/the/data/directory"
-regions = ['JAMSTEC', 'JAMSTEC2', 'NGDC', 'SIO', 'US_multi']
+data_dir = "/cryosat2/ML_DATA/runtime_data"
+regions = ['AGSO', 'JAMSTEC', 'JAMSTEC2', 'NGA', 'NGA2', 'NGDC', 'NOAA_geodas', 'SIO', 'US_multi']
 
-for region in regions:
-    dirname = os.path.join(data_dir, region)
-    ext = ".tsv"
-    filenames = [filename for filename in os.listdir(dirname) if filename.endswith(ext)]
-    random.shuffle(filenames)
-    filenames = [os.path.join(dirname, filename) for filename in filenames]
+def per_region():
+    for region in regions:
+        #dirname = os.path.join(data_dir, region)
+        dirname = data_dir
+        ext = ".tsv.pkl"
+        filenames = [filename for filename in os.listdir(dirname) if
+                region in filename]
+        random.shuffle(filenames)
+        filenames = [os.path.join(dirname, filename) for filename in filenames]
 
-    s0, s1 = int(len(filenames) * 0.15), int(len(filenames) * 0.30)
-    tests, validates, trains = filenames[:s0], filenames[s0:s1], filenames[s1:]
-    for name, dataset in [("test", tests), ("validate", validates), ("train", trains)]:
-        with open("{}-{}.txt".format(region, name), "w") as f:
-            f.write("\n".join(dataset))
+        #s0, s1 = int(len(filenames) * 0.15), int(len(filenames) * 0.30)
+        s0, s1 = int(len(filenames) * 0.25), int(len(filenames) * 0.50)
+        tests, validates, trains = filenames[:s0], filenames[s0:s1],\
+            filenames[s1:]
+        for name, dataset in [("test", tests), ("validate", validates), ("train", trains)]:
+            with open("{}-{}.txt".format(region, name), "w") as f:
+                f.write("\n".join(dataset))
+                f.write("\n")
 
+dirname = data_dir
+ext = ".tsv.pkl"
+filenames = [filename for filename in os.listdir(dirname) if "US_multi2" not in filename]
+random.shuffle(filenames)
+filenames = [os.path.join(dirname, filename) for filename in filenames]
+
+#s0, s1 = int(len(filenames) * 0.25), int(len(filenames) * 0.50)
+s0 = 100
+s1 = 200
+tests, validates = filenames[:s0], filenames[s0:s1]
+for name, dataset in [("test", tests), ("validate", validates)]:
+    with open("{}-{}.txt".format("all", name), "w") as f:
+        f.write("\n".join(dataset))
+        f.write("\n")
+
+train_filenames = filenames[s1:]
+for i in range(10):
+    train_idx = [random.randint(0,len(train_filenames)) for j in range(100)]
+    trains = [train_filenames[id] for id in train_idx]
+    with open("all-train-{}.txt".format(str(i)),"w") as f:
+        f.write("\n".join(trains))
+        f.write("\n")
